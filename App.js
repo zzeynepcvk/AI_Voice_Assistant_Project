3e988a797b55fefad4cb4b00d04290342db31415
zeynep çevik <210541009@firat.edu.tr>
2024-03-31 23:52:59 +0300

    dosya düzeni , NativeWind kurulumu

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text , SafeAreaView } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Ay yaşasınnnnnnnnnnnnnnnnnn !!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
